817741a3f607f89864c293d33c88d656a80a5e72
RMA101 <rhenanderson@Rhens-MacBook-Pro.local>
2016-12-16 14:56:26 -0700

    RGB sliders that will update the view when toggled

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -10,15 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateColor()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        
+        if redSwitch.isOn {
+            red = 1
+        }
+        if greenSwitch.isOn {
+            green = 1
+        }
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        updateColor()
+    }
+    
 
 
 }
